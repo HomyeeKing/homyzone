27c7cbb22c8765abe3eeade8cab2f941d218c31b
HomyeeKing <HomyeeKing@gmail.com>
2021-03-01 15:47:36 +0800

    fix: using optional symbol

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -7,7 +7,7 @@ const routes = router
   .filter(i => i.path.startsWith('/blogs/'))
   .sort(
     (a, b) =>
-      +new Date(b.meta.frontmatter.date) - +new Date(a.meta.frontmatter.date),
+      +new Date(b.meta.frontmatter?.date) - +new Date(a.meta.frontmatter?.date),
   )
 
 </script>
@@ -15,9 +15,9 @@ const routes = router
 <template>
   <div v-for="route in routes" :key="route.name" class="my-5">
     <router-link :to="route.path" class="no-underline">
-      <h2>{{ route.meta.frontmatter.title }}</h2>
-      <small>{{ fromDateString(route.meta.frontmatter.date) }}</small>
-      <small v-if="route.meta.frontmatter.duration">· {{ route.meta.frontmatter.duration }}</small>
+      <h2>{{ route.meta.frontmatter?.title }}</h2>
+      <small>{{ fromDateString(route.meta.frontmatter?.date) }}</small>
+      <small v-if="route.meta.frontmatter?.duration">· {{ route.meta.frontmatter?.duration }}</small>
     </router-link>
   </div>
 </template>
