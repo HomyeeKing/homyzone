7b67d27bbbd736f95c4d6227586d3a1b0b41b756
HomyeeKing <homyeeking@qq.com>
2022-04-09 13:32:30 +0800

    chore: i18n

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,32 +1,22 @@
 <script setup lang="ts">
-import { useI18n } from 'vue-i18n'
 import { isDark } from '@/logics'
 
-const { t, availableLocales, locale } = useI18n()
 
-const toggleLocales = () => {
-  // change to some real logic
-  const locales = availableLocales
-  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
-}
+
 </script>
 
 <template>
   <nav class="text-xl mt-6 text-right">
-    <router-link class="icon-btn mx-2" to="/" :title="t('button.home')">
+    <router-link class="icon-btn mx-2" to="/" title="首页">
       <carbon-campsite />
     </router-link>
 
-    <a class="icon-btn mx-2" :title="t('button.toggle_dark')" @click="isDark = !isDark">
+    <a class="icon-btn mx-2" title="切换深色模式" @click="isDark = !isDark">
       <carbon-moon v-if="isDark" />
       <carbon-sun v-else />
     </a>
 
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
-      <carbon-language />
-    </a>
-
-    <router-link class="icon-btn mx-2" to="/about" :title="t('button.about')">
+    <router-link class="icon-btn mx-2" to="/about" title="关于">
       <carbon-dicom-overlay />
     </router-link>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { useI18n } from 'vue-i18n'
 
 const name = ref('')
 
@@ -11,32 +10,25 @@ const go = () => {
     router.push(`/hi/${encodeURIComponent(name.value)}`)
 }
 
-const { t } = useI18n()
 const navLists = ['blogs', 'movies', 'dramas', 'reading']
 </script>
 
 <template>
   <div class="text-center">
-    <p class="text-4xl ">
+    <p class="text-4xl">
       <carbon-campsite class="inline-block" />
     </p>
     <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        HOMYZONE
-      </a>
+      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">HOMYZONE</a>
     </p>
     <div>
-      <p class="text-sm opacity-75  my-2 underline">
-        {{ t('intro.desc') }}
-      </p>
-      <br>
+      <p class="text-sm opacity-75 my-2 underline">WAY TO GREAT</p>
+      <br />
       <div v-for="nav in navLists" :key="nav">
         <router-link class="text-sm opacity-75 hover:underline" :to="`/${nav}`">
-          <p class="transition duration-150 transform hover:scale-150">
-            {{ t(`cd.${nav}`) }} 👉
-          </p>
+          <p class="transition duration-150 transform hover:scale-150">{{ `cd.${nav}` }} 👉</p>
         </router-link>
-        <br>
+        <br />
       </div>
     </div>
   </div>
