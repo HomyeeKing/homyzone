fbc56333849024d1c94d3fba4c00e2e998907431
HomyeeKing <HomyeeKing@gmail.com>
2026-03-16 23:42:07 +0800

    fix: enable strict mode for Giscus permissions

--- src/components/GiscusComments.vue
+++ src/components/GiscusComments.vue
@@ -27,7 +27,7 @@ const loadGiscus = () => {
   script.setAttribute('data-category', CATEGORY)
   script.setAttribute('data-category-id', CATEGORY_ID)
   script.setAttribute('data-mapping', 'pathname')
-  script.setAttribute('data-strict', '0')
+  script.setAttribute('data-strict', '1')
   script.setAttribute('data-reactions-enabled', '1')
   script.setAttribute('data-emit-metadata', '1')
   script.setAttribute('data-input-position', 'bottom')
