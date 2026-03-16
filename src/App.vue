d7bc576f1c83e10ab63e27e988ffc16ccda361ed
HomyeeKing <HomyeeKing@gmail.com>
2026-03-16 20:02:36 +0800

    fix: allow scrolling on sub-pages while keeping homepage fixed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Nav />
-  <main class="h-screen w-screen overflow-hidden text-gray-700 dark:text-gray-200">
+  <main class="text-gray-700 dark:text-gray-200">
     <router-view />
   </main>
 </template>
